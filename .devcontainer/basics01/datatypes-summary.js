d5b0b9adacc626455a38d503f59f7d75d8e34c61
bettercodevivek <149397884+bettercodevivek@users.noreply.github.com>
2023-11-02 17:30:50 +0000

    summarised datatypes

--- .devcontainer/basics01/datatypes-summary.js
+++ .devcontainer/basics01/datatypes-summary.js
@@ -14,3 +14,31 @@ b)objects
 c)functions
 
 JS is a dynamically typed language*/
+
+/* In JS there are two types of memory:-
+1.Stack Memory
+-> it used in the case of primitive datatype
+
+2.Heap Memory
+-> it is used in case of non-primitive datatype */
+
+let userOne = {
+    email:"hello@gmail.com" ,
+    alias:"devslope"
+}
+
+console.log(userOne)
+
+let userTwo=userOne
+
+console.log(userTwo)
+ 
+userTwo.email="batman@gmail.com"
+
+console.log(userOne)
+console.log(userTwo)
+
+/* kul milake baat yeh hai ki stack memory mein primitve datatypes hai aur usme value ki copy
+banti hai aur changes copy mein hote hai thus altering the copy doesnt alter the actual value
+but heap ke case mein reference of original value milta hai thus altering the value will alter the
+original value*/
