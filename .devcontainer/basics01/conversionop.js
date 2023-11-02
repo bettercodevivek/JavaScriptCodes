378c66d25d9bf817ba9fad3b4f3ae961c4618433
bettercodevivek <149397884+bettercodevivek@users.noreply.github.com>
2023-11-02 07:51:40 +0000

    learned operations

--- .devcontainer/basics01/conversionop.js
+++ .devcontainer/basics01/conversionop.js
@@ -36,4 +36,34 @@ also if string is something like this "34abc" in that case number conversion hap
  1=>true
  0=>false
  " "=>false
- "some string"=>true
+ "some string"=>true*/
+
+/* OPERATIONS */
+
+let str1="hello"
+let str2=" vivek"
+let str3=str1+str2
+
+console.log(str3)
+
+console.log(3+3)
+console.log(8-3)
+console.log(8*3)
+console.log(8%3)
+console.log(2**4)
+
+console.log("2"+2+2)
+
+console.log(2+2+"2")
+
+/* in such cases the first data type in console.log is considered as preferred data type
+means if number value first than whole op to be converted in number but if string value first
+then whole op to be converted in string*/
+
+let a=4
+console.log(++a)
+console.log(a++)
+console.log(a--)
+console.log(--a)
+/* pre-increment means print hone se pehle hi operation hojyega inc/dec ka and post means
+ operation print hone ke baad hoga inc/dec ka*/
