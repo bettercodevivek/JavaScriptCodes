92498a8021c5d9ab2033fac18c93d67d27cfc193
bettercodevivek <149397884+bettercodevivek@users.noreply.github.com>
2023-11-02 08:30:55 +0000

    datatypes-summary

---
+++ .devcontainer/basics01/datatypes-summary.js
@@ -0,0 +1,16 @@
+/* so basically datatypes are only of 2 types :- 
+(on the basis ki how is that datatype stored in the memory)
+1. primitive= they are always call-by-value
+a)string
+b)number
+c)null
+d)undefined
+e)boolean
+f)symbol
+g)BigInt
+2.non-primitive=they are always call-by-reference
+a)array
+b)objects
+c)functions
+
+JS is a dynamically typed language*/
