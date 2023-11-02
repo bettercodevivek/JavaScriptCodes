0e4978c6302d42e59f8c96f9ee69abd9093a17fe
bettercodevivek <149397884+bettercodevivek@users.noreply.github.com>
2023-11-02 05:00:07 +0000

    new file added and variables in javascript learned

--- .devcontainer/basics01/first.js
+++ .devcontainer/basics01/first.js
@@ -1,1 +1,9 @@
-console.log("vivek")
+const accountid=446688
+let accountemail="hello@gmail.com"
+var pwd="12345"
+accountcity="delhi"
+let state;
+/*accountid=222333*/
+console.log(accountid)
+console.table([accountid,accountemail,pwd,accountcity,state])
+/* prefer usage of let over var due to issue in block scope and functional scope*/
