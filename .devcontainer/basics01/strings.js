f5cacd84cf10163fa8403b287e79f9ac2000e23c
bettercodevivek <149397884+bettercodevivek@users.noreply.github.com>
2023-11-02 18:09:09 +0000

    created a new file

---
+++ .devcontainer/basics01/strings.js
@@ -0,0 +1,6 @@
+const name="vivek"
+const repocount=50
+
+console.log(`hello my name is ${name} and my repo count is ${repocount}`)
+
+//this is called string interpolation. it enhances code readability
