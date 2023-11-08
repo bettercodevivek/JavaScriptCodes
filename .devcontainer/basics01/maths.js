d513baca17713bf6431923b883f1c3db18c4879c
Vivek Singh <149397884+bettercodevivek@users.noreply.github.com>
2023-11-08 18:26:15 +0000

    updation in maths.js file

--- .devcontainer/basics01/maths.js
+++ .devcontainer/basics01/maths.js
@@ -39,4 +39,10 @@ console.log(Math.random() * (max-min+1) + min)
 /* math.random() func gives values in the range of 0-1
 but when we multiply this by 10 in some cases there is possibility that values remains under 1 only
 for example 0.01 on multiplication with 10 gives 0.1, so to tackle this problem what we do is to add 1
-thus making the formula => console.log((math.random()*10) +1).*/
+thus making the formula => console.log((math.random()*10) +1).
+
+Now in some cases we want the desired numbers to lie between a certain range in that
+kind of case we define a min and a max and then use this formula
+ console.log((Math.random() *(max-min+1))+min)
+min is added so that we terminate the possibility of number being lesser than min i.e Ki value atleast min se 
+larger ho*/
