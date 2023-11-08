992f24ab65fdbe5aa5e895ad23a0711c84ac2381
Vivek Singh <149397884+bettercodevivek@users.noreply.github.com>
2023-11-08 18:51:16 +0000

    addition of new file

---
+++ .devcontainer/basics01/datetime.js
@@ -0,0 +1,12 @@
+let date1=new Date();
+console.log(date1.toString());
+console.log(date1.toDateString())
+console.log(date1.toLocaleString())
+
+console.log(typeof date1)
+
+let date2=new Date(2023,10,9)
+console.log(date2.toDateString());
+
+let date3=new Date("11-19-2023")
+console.log(date3.toLocaleString())
