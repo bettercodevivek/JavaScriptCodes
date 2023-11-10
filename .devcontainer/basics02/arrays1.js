1bd0f99af44f23ba12e021cb53c6407ffc192ec1
bettercodevivek <viveksingh0520@gmail.com>
2023-11-10 20:02:01 +0530

    updation

--- .devcontainer/basics02/arrays1.js
+++ .devcontainer/basics02/arrays1.js
@@ -8,3 +8,10 @@ arr1.pop();
 console.log(arr1)
 
 //push operations is used to push a value and pop operations deletes the last entered value
+
+const arr2=[4,8,12,16,20]
+arr2.push(24)
+arr2.push(28)
+console.log(arr2)
+arr2.pop()
+console.log(arr2)
