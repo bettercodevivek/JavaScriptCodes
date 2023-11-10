ff51b79bd8257f5d18397d9c9af078aff869422e
bettercodevivek <viveksingh0520@gmail.com>
2023-11-10 20:28:55 +0530

    updation in arrays1 file

--- .devcontainer/basics02/arrays1.js
+++ .devcontainer/basics02/arrays1.js
@@ -15,3 +15,21 @@ arr2.push(28)
 console.log(arr2)
 arr2.pop()
 console.log(arr2)
+
+console.log(arr2.includes(11))
+console.log(arr2.includes(16))
+
+console.log(arr2.indexOf(16))
+
+// lets now talk about two important operations on arrays:- Slice and Splice
+
+const n1=[2,4,6,8,10]
+console.log("A",n1)
+
+const myn1=n1.slice(1,3)
+console.log(myn1)
+console.log("B",n1)
+
+const myn2=n1.splice(1,3)
+console.log(myn2)
+console.log("C",n1)
