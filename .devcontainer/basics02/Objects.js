f74ec35462d674e69ace34188e2c42c4b054c6ee
Vivek Singh <149397884+bettercodevivek@users.noreply.github.com>
2023-11-13 06:45:26 +0000

    updation in objects file

---
+++ .devcontainer/basics02/Objects.js
@@ -0,0 +1,16 @@
+// There are two ways of declaring an object:-
+// 1st=> as literals and 2nd=> as constructors
+// Basically when we declare am object as a constructor it becomes a "singleton"
+
+//*************************************OBJECT LITERALS***********************************//
+
+const obj1={
+    name:"batman",
+    address:"gotham",
+    age:35,
+    email:"batman@yahoo.com",
+    isloggedIn:true
+}
+
+console.log(obj1.name)
+console.log(obj1["address"])
