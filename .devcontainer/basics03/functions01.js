355cfd6e563587acd9733ca89ad86388802540ba
Vivek Singh <149397884+bettercodevivek@users.noreply.github.com>
2023-11-27 17:31:38 +0000

    updation

---
+++ .devcontainer/basics03/functions01.js
@@ -0,0 +1,8 @@
+// In this we will learn about functions and their parameters
+
+function Saymyname(){
+    console.log("Bat")
+    console.log("man")
+}
+
+Saymyname() // this is basically how you execute a function
