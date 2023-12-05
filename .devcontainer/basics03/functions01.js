3d1531747140550b17e50a7a8b5017ed70a856d5
Vivek Singh <149397884+bettercodevivek@users.noreply.github.com>
2023-12-05 05:37:59 +0000

    updation in functions file

--- .devcontainer/basics03/functions01.js
+++ .devcontainer/basics03/functions01.js
@@ -48,3 +48,29 @@ console.log(loginmessage("Vivek"))
 // here !username is equivalent to username===undefined, so when no argument is given we use this
 // if condition
 // This was all about the basics of functions and their parameters
+
+// Now lets consider the cases where the number of parameters for a function is not known 
+// for example:- this might happen in case of a e-commerce website where the number of items
+// the user may add into their cart is unknown but still we need to create a functions that
+// sums up the value of all items
+
+function calculateCartprice(...num1){
+    return num1
+}
+
+console.log(calculateCartprice(200,400,600,800))
+
+// here we use ... (rest operator or in some cases called spread operator), it takes any number of parameters
+
+const user = {
+ username:"vivek",
+ price:999
+}
+
+function handleObject(anyobject){
+    console.log(`the username is ${anyobject.username} and the price is ${anyobject.price} `)
+}
+
+handleObject(user)
+
+// This is how you handle objects in functions.
