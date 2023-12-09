4ba6675511b44ae84d55b6fcd3d051ac74218c01
Vivek Singh <149397884+bettercodevivek@users.noreply.github.com>
2023-12-09 17:26:36 +0000

    updation

---
+++ .devcontainer/basics03/arrow.js
@@ -0,0 +1,17 @@
+//Before starting arrow function lets talk about this keyword.................
+
+const user = {
+username:"vivek",
+price:999,
+
+welcomemessage: function(){
+    console.log(`${this.username}, welcome to the website`)
+    console.log(this)
+}
+}
+
+user.welcomemessage()
+
+user.username = "steve"
+
+user.welcomemessage()
