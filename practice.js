ccb2cca82e2d62d12e5125c01ba95c62ff4e349b
Vivek Singh <149397884+bettercodevivek@users.noreply.github.com>
2024-01-18 14:33:26 +0000

    updation

--- practice.js
+++ practice.js
@@ -1,9 +1,37 @@
 // This is how you declare a variable automatically without using a keyword
 vivek=4;
 console.log(vivek);
-//now,lets use var to declare variables
 
+//now,lets use var to declare variables
 var x=5;
 var y=8;
 var z=x+y;
 console.log(z);
+
+// now,lets use let to declare variables
+let a=20;
+let b=21;
+let c=a+b;
+console.log(c);
+
+// usage of const
+
+const myname='batman';
+//let myname='robin';
+console.log(myname)
+
+/*1. Always declare variables
+
+2. Always use const if the value should not be changed
+
+3. Always use const if the type should not be changed (Arrays and Objects)
+
+4. Only use let if you can't use const
+
+5. Only use var if you MUST support old browsers.
+*/
+
+let carname;
+console.log(typeof carname);
+
+// if you dont define your variable value, then it be undefined.
