7cb7ee5e4f432f9e621b0426fb8485b37e8acedf
Vivek Singh <149397884+bettercodevivek@users.noreply.github.com>
2024-01-18 14:07:59 +0000

    updation

---
+++ practice.js
@@ -0,0 +1,9 @@
+// This is how you declare a variable automatically without using a keyword
+vivek=4;
+console.log(vivek);
+//now,lets use var to declare variables
+
+var x=5;
+var y=8;
+var z=x+y;
+console.log(z);
