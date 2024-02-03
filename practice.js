1a9e8171c450b4caf6d6e7e1c4e67ebe011c09b9
Vivek Singh <149397884+bettercodevivek@users.noreply.github.com>
2024-02-03 08:32:45 +0000

    updation

--- practice.js
+++ practice.js
@@ -590,3 +590,17 @@ if(num11*2>num12){
     else{
        console.log('they are equal boss!!');
     }
+
+    /***********************************JS LOOPS*******************************************/
+
+/*JavaScript supports different kinds of loops:
+for - loops through a block of code a number of times
+for/in - loops through the properties of an object
+for/of - loops through the values of an iterable object
+while - loops through a block of code while a specified condition is true
+do/while - also loops through a block of code while a specified condition is true
+*/
+
+for(let i=1;i<=10;i++){
+    console.log('this loop works!')
+}
